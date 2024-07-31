5e3dc8fcc975d31a87e909ade400fd6313ce4477
Yuhang Song <1595025647@qq.com>
2024-07-31 19:06:32 +0800

    robot exp init

--- logic_ws/src/logic_node/logic_node/embodied_logic.py
+++ logic_ws/src/logic_node/logic_node/embodied_logic.py
@@ -16,7 +16,7 @@ else:
 
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
-from utils.common import text_to_tensor
+from logic_node.utils.common import text_to_tensor
 
 # # ros1
 # from sensor_msgs.msg import PointCloud2, LaserScan,Image
@@ -359,7 +359,7 @@ from gym import spaces
 
 # logic 
 from policy.cma_policy import CMAPolicy
-from utils.common import batch_obs, tokenize_text_nltk
+from logic_node.utils.common import batch_obs, tokenize_text_nltk
 import torch
 
 
