959bb43fb98fdc352f2bf79e2cbe036262e97b40
Yuhang Song <1595025647@qq.com>
2024-08-18 17:57:04 +0800

    cma bsline

--- logic_ws/src/logic_node/logic_node/model/encoders/instruction_encoder.py
+++ logic_ws/src/logic_node/logic_node/model/encoders/instruction_encoder.py
@@ -80,13 +80,14 @@ class InstructionEncoder(nn.Module):
         lengths = (instruction != 0.0).long().sum(dim=2)
         lengths = (lengths != 0.0).long().sum(dim=1).cpu()
 
-        print(f"length {lengths}")
 
         packed_seq = nn.utils.rnn.pack_padded_sequence(
             instruction, lengths, batch_first=True, enforce_sorted=False
         )
 
         output, final_state = self.encoder_rnn(packed_seq)
+
+        print(f"final_state {final_state.shape}")
 
         rnn_type = "LSTM"
         if rnn_type == "LSTM":
