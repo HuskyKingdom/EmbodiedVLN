8707baa4996176ecdc1e3f61591398248f4e50b9
Yuhang Song <1595025647@qq.com>
2024-08-18 16:05:23 +0800

    cma bsline

--- logic_ws/src/logic_node/logic_node/embodied_logic.py
+++ logic_ws/src/logic_node/logic_node/embodied_logic.py
@@ -29,7 +29,7 @@ class MockHabitat:
 
 sys.modules['habitat'] = MockHabitat()
 sys.modules['habitat.config'] = MockHabitat()
-sys.modules['habitat.config.defult'] = MockHabitat()
+sys.modules['habitat.config.default'] = MockHabitat()
 
 
 # # ros1
